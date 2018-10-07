6953f00ee6c7b652af9bd5bf918c55030129128b
gekoramy <luka.mosetti@gmail.com>
2018-10-07 12:11:45 +0200

    Folds(n) = Power(n) - 1

--- src/main/java/Cutter.java
+++ src/main/java/Cutter.java
@@ -2,22 +2,21 @@ import java.math.BigInteger;
 
 public class Cutter {
 
-    private final Folds folds = new Folds();
     private final Power power = new Power();
 
-    BigInteger cutHorizontal(Paper paper) {
+    public BigInteger cutHorizontal(Paper paper) {
         if (paper.t == 0 && paper.b == 0)
             return BigInteger.valueOf(2);
 
         final int horizontal = paper.t + paper.b;
-        return power.get(horizontal + 1).subtract(BigInteger.valueOf(folds.get(horizontal)));
+        return power.get(horizontal + 1).subtract(power.get(horizontal).subtract(BigInteger.ONE));
     }
 
-    BigInteger cutVertical(Paper paper) {
+    public BigInteger cutVertical(Paper paper) {
         if (paper.r == 0 && paper.l == 0)
             return BigInteger.valueOf(2);
 
         final int vertical = paper.r + paper.l;
-        return power.get(vertical + 1).subtract(BigInteger.valueOf(folds.get(vertical)));
+        return power.get(vertical + 1).subtract(power.get(vertical).subtract(BigInteger.ONE));
     }
 }
